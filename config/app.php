9f0ac51d8dc336422f79b05d3ba7fdfce1122c25
Hector Miranda Conza <hecar87@gmail.com>
2026-04-26 00:45:59 -0500

    all ok

--- config/app.php
+++ config/app.php
@@ -127,6 +127,9 @@ return [
 		// App\Providers\BroadcastServiceProvider::class,
 		App\Providers\EventServiceProvider::class,
 		App\Providers\RouteServiceProvider::class,
+
+		// Modules
+		App\Modules\State\Providers\StateServiceProvider::class,
 	])->toArray(),
 
 	/*
