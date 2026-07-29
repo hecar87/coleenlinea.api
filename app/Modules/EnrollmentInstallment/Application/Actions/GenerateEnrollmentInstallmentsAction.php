4bb7b8176e78c245ad00114680f5d2b8cbd0538f
Hector Miranda Conza <hecar87@gmail.com>
2026-07-29 11:51:48 -0500

    Create base

---
+++ app/Modules/EnrollmentInstallment/Application/Actions/GenerateEnrollmentInstallmentsAction.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Modules\EnrollmentInstallment\Application\Actions;
+
+use Illuminate\Support\Facades\DB;
+use App\Helpers\Result;
+use App\Helpers\ResultManager;
+
+use App\Modules\EnrollmentInstallment\Domain\Repositories\IEnrollmentInstallmentRepository;
+use App\Modules\Enrollment\Domain\Repositories\IEnrollmentRepository;
+use App\Modules\TypeCurrency\Domain\Repositories\ITypeCurrencyRepository;
+use App\Modules\TypeInstallment\Domain\Repositories\ITypeInstallmentRepository;
+
+use App\Modules\EnrollmentInstallment\Application\DTOs\CreateEnrollmentInstallmentDTO;
+use App\Modules\EnrollmentInstallment\Application\DTOs\DuplicatedEnrollmentInstallmentDTO;
+
+
+class CreateEnrollmentInstallmentAction
+{
+
+	public function __construct(
+		protected IEnrollmentInstallmentRepository $oEnrollmentInstallmentRepository,
+		protected IEnrollmentRepository $oEnrollmentRepository,
+		protected ITypeInstallmentRepository $oTypeInstallmentRepository,
+		protected ITypeCurrencyRepository $oTypeCurrencyRepository
+	)
+	{
+	}
+
+	public function execute(CreateEnrollmentInstallmentDTO $oData) : Result
+	{
+		//------------------------------------------------------------------------------
+		//	VARIABLES
+		//------------------------------------------------------------------------------
+		$oEntity = $this->oEnrollmentInstallmentRepository->getEntity();
+		$oDataDuplicated = new DuplicatedEnrollmentInstallmentDTO(
+			Id_EnrollmentInstallment	: $oData->Id_EnrollmentInstallment,
+			EnrollmentInstallment_Order	: $oData->EnrollmentInstallment_Order,
+			Id_Enrollment				: $oData->Id_Enrollment,
+			Id_TypeCurrency				: $oData->Id_TypeCurrency,
+			Id_TypeInstallment			: $oData->Id_TypeInstallment
+		);
+
+
+		//------------------------------------------------------------------------------
+		//	FUNCTION
+		//------------------------------------------------------------------------------
+		try
+		{
+			//
+			//	TRANSACTION
+			//
+			DB::beginTransaction();
+
+			$oResult = $this->oEnrollmentRepository->exists($oData->Id_Enrollment);
+			if ( $oResult->RESULT_STS <> 200 ){ DB::rollBack(); return $oResult; }
+
+			$oResult = $this->oTypeCurrencyRepository->exists($oData->Id_TypeCurrency);
+			if ( $oResult->RESULT_STS <> 200 ){ DB::rollBack(); return $oResult; }
+
+			$oResult = $this->oTypeInstallmentRepository->exists($oData->Id_TypeInstallment);
+			if ( $oResult->RESULT_STS <> 200 ){ DB::rollBack(); return $oResult; }
+
+
+			$oResult = $this->oEnrollmentInstallmentRepository->duplicated($oDataDuplicated);
+			if ( $oResult->RESULT_STS <> 200 ){ DB::rollBack(); return $oResult; }
+
+			$oResult = $this->oEnrollmentInstallmentRepository->create($oData);
+			if ( $oResult->RESULT_STS <> 200 ){ DB::rollBack(); return $oResult; }
+
+			DB::commit();
+		}
+		catch (\Exception $oException)
+		{
+			DB::rollBack();
+			$oResult 	= ResultManager::Result(2000, $oEntity, null, 0, $oException->getMessage());
+		}
+
+
+		//------------------------------------------------------------------------------
+		//	RESPONSE
+		//------------------------------------------------------------------------------
+		return $oResult;
+
+	}
+}
---
+++ app/Modules/EnrollmentInstallment/Application/DTOs/GenerateEnrollmentInstallmentsDTO.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Modules\EnrollmentInstallment\Application\DTOs;
+
+use Illuminate\Http\Request;
+
+class CreateEnrollmentInstallmentDTO
+{
+    public function __construct(
+        public int $Id_EnrollmentInstallment,
+        public int $EnrollmentInstallment_Order,
+		public string $EnrollmentInstallment_Description,
+		public float $EnrollmentInstallment_Amount_Budgeted,
+		public float $EnrollmentInstallment_Amount_Discounted,
+		public float $EnrollmentInstallment_Amount_Payabled,
+		public string $EnrollmentInstallment_Date_Collection,
+		public string $EnrollmentInstallment_Date_Due,
+		public int $EnrollmentInstallment_Required,
+		public int $Id_Enrollment,
+		public int $Id_TypeCurrency,
+		public int $Id_TypeInstallment
+    ) {}
+
+    public static function fromRequest(Request $oRequest) : self
+    {
+        return new self(
+            Id_EnrollmentInstallment: (int) $oRequest->input("Id_EnrollmentInstallment", 0),
+            EnrollmentInstallment_Order: (int) $oRequest->input("EnrollmentInstallment_Order", 0),
+            EnrollmentInstallment_Description: $oRequest->input("EnrollmentInstallment_Description", 0),
+            EnrollmentInstallment_Amount_Budgeted: (float) $oRequest->input("EnrollmentInstallment_Amount_Budgeted", 0),
+            EnrollmentInstallment_Amount_Discounted: (float) $oRequest->input("EnrollmentInstallment_Amount_Discounted", 0),
+            EnrollmentInstallment_Amount_Payabled: (float) $oRequest->input("EnrollmentInstallment_Amount_Payabled", 0),
+            EnrollmentInstallment_Date_Collection: $oRequest->input("EnrollmentInstallment_Date_Collection", 0),
+            EnrollmentInstallment_Date_Due: $oRequest->input("EnrollmentInstallment_Date_Due", 0),
+            EnrollmentInstallment_Required: (int) $oRequest->input("EnrollmentInstallment_Required", 0),
+            Id_Enrollment: (int) $oRequest->input("Id_Enrollment", 0),
+            Id_TypeCurrency: (int) $oRequest->input("Id_TypeCurrency", 0),
+            Id_TypeInstallment: (int) $oRequest->input("Id_TypeInstallment", 0)
+        );
+    }
+}
